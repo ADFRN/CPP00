19f9bc12a3adbdc7fd5cd44e7141070453a77ee8
Adrien Fournier <adrien.fournier1998@hotmail.fr>
2026-05-22 10:04:21 +0200

    look ok for ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,25 +6,11 @@
 /*   By: afournie <afournie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/13 11:09:19 by afournie          #+#    #+#             */
-/*   Updated: 2026/05/21 17:48:25 by afournie         ###   ########.fr       */
+/*   Updated: 2026/05/22 10:00:17 by afournie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
-
-// std::string trim(const std::string &str)
-// {
-// 	size_t start = 0;
-// 	size_t end = str.length();
-
-// 	while (start < end && std::isspace(str[start]))
-// 		start++;
-
-// 	while (end > start && std::isspace(str[end - 1]))
-// 		end--;
-
-// 	return str.substr(start, end - start);
-// }
 
 int	main(void)
 {
